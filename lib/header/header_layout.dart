5e34765d9f2832cc1edf8be1cc059ce8002a0666
poeticdev23 <poeticdev23@gmail.com>
2025-03-11 09:32:54 +0900

    Feat: 헤더 터치시 다음 메세지 표시

--- lib/header/header_layout.dart
+++ lib/header/header_layout.dart
@@ -199,11 +199,24 @@ class _HeaderLayoutState extends ConsumerState<HeaderLayout>
       );
     }
 
-    return SizedBox(
-      height: widget.height,
-      child: FadeTransition(
-        opacity: _fadeController,
-        child: children[_currentIndex],
+    return GestureDetector(
+      onTap: () {
+        if (!_fadeController.isAnimating) {
+          _stopAutoSlide();
+          _fadeController.reverse().then((_) {
+            _fadeController.forward();
+            Future.delayed(Duration(seconds: 12), () {
+              _startAutoSlide();
+            });
+          });
+        }
+      },
+      child: SizedBox(
+        height: widget.height,
+        child: FadeTransition(
+          opacity: _fadeController,
+          child: children[_currentIndex],
+        ),
       ),
     );
   }
