443af5b13af1d4cfe5307a9002a24781ce60e99d
poeticdev23 <poeticdev23@gmail.com>
2025-11-12 09:58:30 +0900

    Fix(Riverpod): Riverpod 2.x.x -> 3.x.x migration

--- lib/common/util/data/updaters.dart
+++ lib/common/util/data/updaters.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final timetableUpdater = StateProvider<DateTime>((ref) {
